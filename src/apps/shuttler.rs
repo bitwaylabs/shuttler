f06031714c30a21d4897e40e65e285617ce072e3
Lazy Luis <ping@side.one>
2025-07-11 14:22:59 +0800

    add message id

--- src/apps/shuttler.rs
+++ src/apps/shuttler.rs
@@ -258,9 +258,9 @@ impl<'a> Shuttler<'a> {
                     self.handle_missed_bridge_signing_request(&mut context).await;
                 }
                 swarm_event = context.swarm.select_next_some() => match swarm_event {
-                    SwarmEvent::Behaviour(ShuttlerBehaviourEvent::Gossip(gossipsub::Event::Message{ message, propagation_source, .. })) => {
+                    SwarmEvent::Behaviour(ShuttlerBehaviourEvent::Gossip(gossipsub::Event::Message{ message, propagation_source, message_id })) => {
                         update_received_heartbeat(&context, &message);
-                        tracing::debug!("propagation source: {:?}", propagation_source);
+                        tracing::debug!("propagation source: {:?}, {:?}", propagation_source,  message_id);
                         metrics::counter!("recieved_messages", "sender"=> message.source.unwrap_or(propagation_source).to_string()).increment(1);
                         for app in &self.apps {
                             dispatch_messages(app, &mut context, &message);
