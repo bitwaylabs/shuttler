bfe6d19c9d5df67673bf0aec8ba6c4b45e763c74
Lazy Luis <ping@side.one>
2025-07-12 09:53:52 +0800

    optimize libp2p parameters

--- src/apps/shuttler.rs
+++ src/apps/shuttler.rs
@@ -64,10 +64,10 @@ fn initial_swarm(keybyte: impl AsMut<[u8]>) -> Swarm<ShuttlerBehaviour> {
 
             // Set a custom gossipsub configuration
             let gossipsub_config = gossipsub::ConfigBuilder::default()
-                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+                // .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
                 // .validate_messages() // This sets the kind of message validation. The default is Strict (enforce message signing)
                 .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
-                .duplicate_cache_time(Duration::from_secs(60*60*2))
+                .duplicate_cache_time(Duration::from_secs(10*60))
                 .max_transmit_size(512000)
                 // .gossip_retransimission(10) // default is 3
                 .flood_publish(false)
