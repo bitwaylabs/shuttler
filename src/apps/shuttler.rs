2a0dc12f6162f381cb9bcf62bf6a519239083d41
Lazy Luis <ping@side.one>
2025-09-29 22:07:26 +0800

    add timestamp

--- src/apps/shuttler.rs
+++ src/apps/shuttler.rs
@@ -67,7 +67,7 @@ fn initial_swarm(keybyte: impl AsMut<[u8]>) -> Swarm<ShuttlerBehaviour> {
                 // .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
                 // .validate_messages() // This sets the kind of message validation. The default is Strict (enforce message signing)
                 .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
-                .duplicate_cache_time(Duration::from_secs(10*60))
+                .duplicate_cache_time(Duration::from_secs(177))
                 .max_transmit_size(512000)
                 // .gossip_retransimission(10) // default is 3
                 .flood_publish(false)
