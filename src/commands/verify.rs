6c5bead28aeb9fc164f4e02d22addcf7523ac8f8
Lazy Luis <ping@side.one>
2025-10-01 18:36:32 +0800

    update

--- src/commands/verify.rs
+++ src/commands/verify.rs
@@ -4,12 +4,14 @@ use crate::{config::{Config, VaultKeypair}, helper::store::{DefaultStore, Store}
 pub async fn execute(home: &str, store_key: String, pubkey: String) {
 
     let conf = Config::from_file(home).unwrap();
-    let keystore: DefaultStore<String, VaultKeypair> = DefaultStore::new(conf.get_database_with_name("keystore"));
+    let keystore: DefaultStore<String, VaultKeypair> = DefaultStore::new(conf.get_database_with_name("keypairs"));
 
-    let keypair = keystore.get(&store_key).expect("Key not found.");
+    println!("home: {:?}", home,);
+    let keypair = keystore.get(&store_key).expect("key not found");
 
     let local_pubkey = keypair.pub_key.verifying_key().serialize().expect("Serialize failure");
 
     println!("pubkey: {:?}, {:?}", local_pubkey, pubkey);
+
     
 }
